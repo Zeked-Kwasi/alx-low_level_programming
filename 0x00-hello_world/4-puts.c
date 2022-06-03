f681ead154ec4ad429db162c42206d3df9a1a0db
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-06-03 12:37:50 +0000

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
