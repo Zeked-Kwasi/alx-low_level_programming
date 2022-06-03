74cdc5052b0ee82b1ae495227d821cbde2d1b726
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-06-03 15:15:37 +0000

    Advanced Eight Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - gives the output
+ *
+ *Return: Gives 1 when error is found
+ *
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
