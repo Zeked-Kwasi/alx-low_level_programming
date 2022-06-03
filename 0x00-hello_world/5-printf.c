223d6c4b2d987f973827343932fa521a8e4a7285
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-06-03 12:56:30 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints a text
+ *
+ *Return: Always gives 0 when code is successful.
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
