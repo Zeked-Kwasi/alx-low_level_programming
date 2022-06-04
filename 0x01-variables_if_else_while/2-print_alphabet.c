8ce73bed23a4ecbc94191a32adf62e2dd98394f3
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-06-04 04:46:25 +0000

    Print alphabets with putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  */
 
 int main(void)
-{	char alpha;
+{	char c;
 
-	for (alpha = "a"; alpha <= "z"; ++alpha)
-	putchar(alpha);
+	for (c = "a"; c <= "z"; ++c)
+	putchar(c);
 	putchar("\n");
 
 	return (0);
