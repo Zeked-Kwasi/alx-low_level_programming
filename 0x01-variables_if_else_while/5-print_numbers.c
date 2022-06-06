a625915da464553590fbf4684584e3d6f014a3c8
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-06-06 10:50:36 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - code entry
+*
+* Return: 0 means success
+*/
+
+int main(void)
+{
+	char num_char:
+
+	for (num_char = '0'; num_char <= '9'; ++num_char)
+	{
+		putchar(num_char);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
