8845191d423e6c3e799c53fa6e70de90ab234804
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-06-06 10:03:29 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print-alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - code entry
+*
+* Return: 0 means success
+*/
+
+int main(void)
+{
+	char some_alpha;
+
+	for (some_alpha = 'a'; some_alpha <= 'z'; ++some_alpha)
+	{
+		if (some_alpha != 'e' && some_alpha != 'q')
+		{
+			putchar(some_alpha);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
