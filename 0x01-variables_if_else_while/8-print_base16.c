363795cca26937bdfc86d115f870152bd4d53968
MelchiZedek17 <eugeneprahamagyei@gmail.com>
2022-06-19 08:02:16 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Code entry
+ *
+ * Return: 0 means success
+ */
+
+int main(void)
+{
+char hexdigit;
+
+for (hexdigit = '0'; hexdigit <= '9'; ++hexdigit)
+}
+	putchar(hexdigit);
+}
+
+for (hexdigit = 'a'; hexdigit <= 'f'; ++hexdigit)
+{
+	putchar(hexdigit);
+}
+
+putchar('\n');
+
+return (0);
+
+}
