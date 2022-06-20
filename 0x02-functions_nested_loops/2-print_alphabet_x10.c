9316a753b69a7a16e7be65980148a31d2274f53a
MelchiZedek17 <eugeneprahamagyei@gmail.com>
2022-06-20 05:27:02 +0000

    Print 10x tha alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - code entry
+ *
+ * Return - empty
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+char alpha;
+char freq;
+
+for (freq = '0'; freq < 10; ++freq)
+{
+	for (alpha = 'a'; alpha <= 'z'; ++alpha)
+	{
+		_putchar(alpha);
+	}
+
+	_putchar('\n');
+}
+
+}
