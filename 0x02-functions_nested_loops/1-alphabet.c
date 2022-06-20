929d27c67b0fdb46dcf0fad7a01f87a224f5de3c
MelchiZedek17 <eugeneprahamagyei@gmail.com>
2022-06-20 05:04:51 +0000

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - code entry
+ *
+ * Return: empty
+ *
+ */
+void print_alphabet(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+{
+	_putchar(alpha);
+}
+
+_putchar('\n');
+
+}
+
