b37e9bb9905502a0ec26ee3e6ac49641a7b9fe75
MelchiZedek17 <eugeneprahamagyei@gmail.com>
2022-06-29 16:18:38 +0000

    Check Alphabet Characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabet letters
+ *
+ * @alpha: alphabet checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int alpha)
+{
+if ((alpha >= 'a' && alpha <= 'z') || (alpha >= 'A' && alpha <= 'Z'))
+	return (1);
+else
+	return (0);
+}
