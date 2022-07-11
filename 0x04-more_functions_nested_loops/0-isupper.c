38fa2cf91d15898a383aed0dd0a946d38e57f109
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-07-11 12:46:50 -0700

    Task Zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - code entry
+ * @upper: alphabets to be checked
+ *
+ * Return: 1 0r 0
+ */
+
+int _isupper(int upper)
+{
+if (upper >= 65 && upper <= 90)
+{
+	return (1);
+}
+
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+My readme for 0x04-more_functions_nested_loops project
