913795f700a454124e7eac36d84c0ee02daac6d1
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-07-12 00:19:22 -0700

    Task Two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - code entry
+ * @num1: 1st number
+ * @num2: 2nd number
+ *
+ * Return: result of mul function
+ */
+
+int mul(int num1, int num2)
+{
+return (num1 * num2);
+}
