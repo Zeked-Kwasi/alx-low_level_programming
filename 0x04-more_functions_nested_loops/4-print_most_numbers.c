793d53420c53fa5e09424511370503158c383dfc
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-07-12 04:56:34 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - code entry
+ *
+ * Return: 0 means success
+ */
+
+void print_most_numbers(void)
+{
+int num = 0;
+
+for (; num <= 9; num++)
+{
+	if (num == 2 || num == 4)
+	{
+		continue;
+	}
+	else
+	{
+		_putchar(num + '0');
+	}
+}
+
+_putchar('\n');
+}
