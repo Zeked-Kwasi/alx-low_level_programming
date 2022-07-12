797134f3bda6fe20ca842e59049ddf14e5e9c027
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-07-12 06:15:03 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - code entry
+ *
+ * Return: success
+ */
+
+void more_numbers(void)
+{
+int num1, num2;
+for (num1 = 0; num1 < 10; num1++)
+{
+	for (num2 = 0; num2 <= 14; num2++)
+	{
+		if (num2 > 9)
+		{
+			_putchar((num2 / 10) + '0');
+		}
+
+		_putchar((num2 % 10) + '0');
+	}
+
+	_putchar('\n');
+}
+
+}
